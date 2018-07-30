59847fe3e8cd5b780b6c25ba231dbc7a12e47d10
Sergio Touset <sergio.touset@intelygenz.com>
2018-07-30 10:03:14 +0200

    add home

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-btn>nueva partida</v-btn>
+    <v-btn>continuar partida</v-btn>
+    <v-btn>partidas anteriores</v-btn>
+    <v-btn>about</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
