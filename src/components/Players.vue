7f662c37b7ca6ed46110aa1501aad2019eb1f4cb
sergio igz <sergio.touset@intelygenz.com>
2020-01-28 17:37:53 +0100

    New: add prettier - eslint configuration

---
+++ src/components/Players.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul class="players-list">
+      <li v-for="(player, index) in players" :key="index">{{player}}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Players',
+  data() {
+    return {
+      players: ['Kami', 'Paloma', 'Roma']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.players-list {
+  list-style: none;
+  padding: 0;
+}
+.players-list li {
+  padding: 8px;
+}
+</style>
